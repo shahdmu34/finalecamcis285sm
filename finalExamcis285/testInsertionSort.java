2b0e926de49108a798e1ef430e5ef85a9ab40695
Shahd Mustafa <shahdmu@umich.edu>
2022-04-26 16:34:27 -0400

    commit mixed

--- finalExamcis285/testInsertionSort.java
+++ finalExamcis285/testInsertionSort.java
@@ -8,48 +8,15 @@ class testInsertionSort {
 
 	@Test
 	public void test() {
-	
+
 		testPositive();
 		testNegative();
 		testMixed();
-		
+
 	}
 	 public testInsertionSort() {
 	    }
-	 public void testPositive(){
-	        int[] arr = new int[5];
-	        arr[0] = 8;
-	        arr[1] = 9;
-	        arr[2] = 6;
-	        arr[3] = 1;
-	        arr[4] = 10;
-
-	        int[] Sortedarr = new int[5];
-	        Sortedarr[0] = 1;
-	        Sortedarr[1] = 6;
-	        Sortedarr[2] = 8;
-	        Sortedarr[3] = 9;
-	        Sortedarr[4] = 10;
-	        int[] result = new InsertionSort().basicInsertionSort(arr);
-	        assertArrayEquals(Sortedarr, result);
-}
-	  public void testNegative(){
-	    	 int[] arr = new int[5];
-	         arr[0] = -8;
-	         arr[1] = -9;
-	         arr[2] = -6;
-	         arr[3] = -1;
-	         arr[4] = -10;
-	         
-	         int[] Sortedarr = new int[5];
-	         Sortedarr[0] = -10;
-	         Sortedarr[1] = -9;
-	         Sortedarr[2] = -8;
-	         Sortedarr[3] = -6;
-	         Sortedarr[4] = -1;
-	         int[] result = new InsertionSort().basicInsertionSort(arr);
-		    assertArrayEquals(Sortedarr, result);
-	  }
+	 
 	   public void testMixed(){
 	    	 int[] arr = new int[5];
 	         arr[0] = -9;
@@ -57,7 +24,7 @@ class testInsertionSort {
 	         arr[2] = 2;
 	         arr[3] = 8;
 	         arr[4] = 10;
-	         
+
 	         int[] Sortedarr = new int[5];
 	         Sortedarr[0] = -9;
 	         Sortedarr[1] = -7;
